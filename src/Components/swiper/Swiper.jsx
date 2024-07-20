3a1fab16956eb2353489089a1e6cebc7428f7545
Akash Chaudhary <akashchaudhary0201@gmail.com>
2024-07-20 22:25:53 +0530

    All Done from the developer's side

--- src/Components/swiper/Swiper.jsx
+++ src/Components/swiper/Swiper.jsx
@@ -8,14 +8,14 @@
 //   const [categories, setCategories] = useState([]);
 
 //   useEffect(() => {
-//     let fectCategories = async () => {
+//     const fetchCategories = async () => {
 //       let response = await fetch(
 //         "https://vashnoi-handloom-cms.vercel.app/api/resources/categories"
 //       );
 //       let data = await response.json();
 //       setCategories(data);
 //     };
-//     fectCategories();
+//     fetchCategories();
 //   }, []);
 
 //   const settings = {
@@ -43,39 +43,36 @@
 //   };
 
 //   return (
-//     <div>
-//       <div>
-//         <Slider {...settings}>
-//           {categories.map((c) => (
-//             <div
-//               key={c.id}
-//               className="bg-gradient-to-b from-[#3d6c6e] to-white rounded-sm p-[20px]"
-//             >
-//               <div className="flex  justify-center m-auto items-center">
-//                 <img
-//                   src={c.imageUrl}
-//                   className="h-44 w-44 m-auto object-cover rounded-full "
-//                   alt={c.name}
-//                 />
-//               </div>
-//               <div className="">
-//                 <h1 className="text-black text-[20px] mt-1">{c.name}</h1>
-//                 <Link to={`/category/${c.id}`}>
-//                   <button className="bg-[#006467] px-10 text-white py-2 mt-6 rounded-lg">
-//                     See Products
-//                   </button>
-//                 </Link>
-//               </div>
+//     <div className="overflow-hidden">
+//       <Slider {...settings}>
+//         {categories.map((c) => (
+//           <div
+//             key={c.id}
+//             className="bg-gradient-to-b from-[#3d6c6e] to-white rounded-sm p-[20px] overflow-hidden"
+//           >
+//             <div className="flex justify-center m-auto items-center">
+//               <img
+//                 src={c.imageUrl}
+//                 className="h-44 w-44 m-auto object-cover rounded-full"
+//                 alt={c.name}
+//               />
 //             </div>
-//           ))}
-//         </Slider>
-//       </div>
+//             <div>
+//               <h1 className="text-black text-[20px] mt-1">{c.name}</h1>
+//               <Link to={`/category/${c.id}`}>
+//                 <button className="bg-[#006467] px-10 text-white py-2 mt-6 rounded-lg">
+//                   See Products
+//                 </button>
+//               </Link>
+//             </div>
+//           </div>
+//         ))}
+//       </Slider>
 //     </div>
 //   );
 // };
 
 // export default Swiper;
-
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -122,7 +119,7 @@ const Swiper = () => {
 
   return (
     <div className="overflow-hidden">
-      <Slider {...settings}>
+      <Slider {...settings} key={categories.length}>
         {categories.map((c) => (
           <div
             key={c.id}
